70b6b095bdfe8e825a493db87e40158940ead3c2
Liardnos <florent.loock@gmail.com>
2024-04-25 18:56:35 +0200

    [~] basic stuff

--- src/bde.cpp
+++ src/bde.cpp
@@ -2,9 +2,12 @@
 #include <fstream>
 #include <filesystem>
 #include <iostream>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "lib/json.hpp"
 #include "bde.hpp"
+#include <pty.h>
 
 Bde::Bde() {
 }
@@ -31,8 +34,55 @@ bool Bde::checkForBderc() {
 void Bde::exec() {
     if (!checkForBderc())
         return;
-    std::cout << "starting new bash" << std::endl;
-    exit(system("bash"));
+
+    int fd[2];
+    // pipe(fd);
+
+    // int cin = dup(0);
+    // int cout = dup(1);
+    // int cerr = dup(2);
+    
+
+    int master;
+    int pid = forkpty(&master, NULL, NULL, NULL);
+    std::cout << "pid: " << pid << std::endl;
+    // int pid = fork();
+    if (pid == 0) {
+        std::cout << "child" << std::endl;
+        std::cout << "starting new bash" << std::endl;
+        //set up new fds
+        // dup2(pipefd[1], 0);
+        // exit(execve("/bin/bash", (char *const *)envp, nullptr));
+        // close(fd[1]);
+		// dup2(fd[0], 0);
+
+        char * const * bashArgs = { NULL };
+        int r = execvp("bash", bashArgs);
+        // write(0, "end\n", 4);
+        // exit(r);
+        return;
+    } else {
+        close(fd[0]);
+        while (1) {
+            char buff[2048];
+            ssize_t s = read(0, buff, 2048);
+            if (s <= 0)
+                break;
+            std::cout << "write to master" << std::endl;
+            write(master, buff, s);
+        }
+        // dup2(fd[1], 0);
+        waitpid(pid, nullptr, 0);
+        // close(fd[1]);
+        // restore original fds
+        // dup2(cin, 0);
+        std::cout << "resuming previous bash" << std::endl;
+        // dup2(cout, 1);
+        // dup2(cerr, 2);
+    }
+    // close(cin);
+    // close(cout);
+    // close(cerr);
 }
 
 void Bde::allow() {
--- src/main.cpp
+++ src/main.cpp
@@ -15,18 +15,20 @@ int main(int argc, char const *argv[]) {
         } else if (argv[1] == std::string("isallowed")) {
             return !shellEnv.checkForBderc();
         } else if (argv[1] == std::string("hook")) {
-            std::cout << "_bde_hook() {" << std::endl;
-            std::cout << "    local previous_exit_status=$?;" << std::endl;
-            std::cout << "    bde exec;" << std::endl;
-            std::cout << "    return $previous_exit_status;" << std::endl;
-            std::cout << "};" << std::endl;
-            std::cout << "if ! [[ \"${PROMPT_COMMAND:-}\" =~ _bde_hook ]]; then" << std::endl;
-            std::cout << "  PROMPT_COMMAND=\"_bde_hook${PROMPT_COMMAND:+;$PROMPT_COMMAND}\"" << std::endl;
-            std::cout << "fi" << std::endl;
-            std::cout << "" << std::endl;
-            std::cout << "if (bde isallowed); then" << std::endl;
-            std::cout << "source ./.bderc" << std::endl;
-            std::cout << "fi" << std::endl;
+            // std::cout << "_bde_hook() {" << std::endl;
+            // std::cout << "    local previous_exit_status=$?;" << std::endl;
+            // std::cout << "    bde exec;" << std::endl;
+            // std::cout << "    return $previous_exit_status;" << std::endl;
+            // std::cout << "};" << std::endl;
+            // std::cout << "if ! [[ \"${PROMPT_COMMAND:-}\" =~ _bde_hook ]]; then" << std::endl;
+            // std::cout << "  PROMPT_COMMAND=\"_bde_hook${PROMPT_COMMAND:+;$PROMPT_COMMAND}\"" << std::endl;
+            // std::cout << "fi" << std::endl;
+            // std::cout << "" << std::endl;
+
+
+            // std::cout << "if (bde isallowed); then" << std::endl;
+            // std::cout << "source ./.bderc" << std::endl;
+            // std::cout << "fi" << std::endl;
             return 0;
         } else {
             std::cout << "Invalid argument" << std::endl;
